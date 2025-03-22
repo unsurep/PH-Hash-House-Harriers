bd255bbbec67d5c5be20f94a5b8dd7e43a1a52f3
Oghenevwede Louis Umukoro <168831499+unsurep@users.noreply.github.com>
2025-03-22 14:56:46 +0100

    z-50 on scroll

--- app/page.jsx
+++ app/page.jsx
@@ -13,8 +13,8 @@ const Home = () => {
         <script defer src="https://app.fastbots.ai/embed.js" data-bot-id="cm7pttc0e0z9xrik5y9zyqtx8"></script>
       </div>
 
-      <Link href='#top'>
-        <div className="w-[5px] rounded-full px-6 py-4 bg-[#3177A3] items-center justify-center ring-8 hover:ring-red-300   flex fixed left-5 bottom-5"><span className="font-bold "><FaArrowUp /></span></div>
+      <Link href='#top '>
+        <div className="w-[5px] rounded-full px-6 py-4 bg-[#3177A3] items-center justify-center ring-8 hover:ring-red-300 z-50  flex fixed left-5 bottom-5"><span className="font-bold "><FaArrowUp /></span></div>
         </Link>
       
     </div>
