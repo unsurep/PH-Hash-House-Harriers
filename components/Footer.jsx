2a00ba4d62b06c8a310cccca31c07db7d756b99f
Oghenevwede Louis Umukoro <168831499+unsurep@users.noreply.github.com>
2025-03-22 15:01:28 +0100

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -98,7 +98,7 @@ const Footer = () => {
         </footer>
 
          {/* my brand */}
-            <div className="flex justify-center items-center py-3 gap-3 px-[1rem] md:px-[3rem]">
+            <div className="flex justify-center items-center py-6 md:py-3 gap-3 px-[1rem] md:px-[3rem]">
                 <h1 className="text-sm ">© 2025 Port Harcourt Hash House Harriers. | All Rights Reserved
                 | Developer: 1-SurePlayer </h1>
 
